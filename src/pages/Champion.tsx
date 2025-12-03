11b5dd499f914e7c9e2695b1120b582f2b7aed54
KevinVigne <kev.vigne@gmail.com>
2025-12-03 16:48:48 +0100

    Création de la page de l'affichage d'un champion

--- src/pages/Champion.tsx
+++ src/pages/Champion.tsx
@@ -5,59 +5,59 @@ import type { Champion } from "../types/ChampionType";
 export function Champion() {
 
     const [championList , setChampionList] = useState<Champion[]>([]);
-    const [champion , setChampion] = useState<Champion[]>([]);
+    const [champion , setChampion] = useState<Champion>();
     
     useEffect(()=>{
         const getAllChampions = async () =>{
-            const ALL_CHAMPION_URL = "https://ddragon.leagueoflegends.com/cdn/15.23.1/data/fr_FR/champion.json"
+            const ALL_CHAMPIONS_URL = "https://ddragon.leagueoflegends.com/cdn/15.23.1/data/fr_FR/champion.json"
             try {
-                const allChampionData = await axios.get(ALL_CHAMPION_URL);
-                //Récupère les data 
-                let championList:Champion [] = Object.values(allChampionData.data.data);
+                const allChampionsData = await axios.get(ALL_CHAMPIONS_URL);
 
-                //Initialise  le tableau avec les valeur des data de l'API Riot
+                //Récupère la liste des champions 
+                let championList:Champion [] = Object.values(allChampionsData.data.data);
+
+                
                 setChampionList(championList);
             } catch (error) {
                 console.log("Erreur lors du chargement des champions", error);
             }
         }
-        //Lancement de la fonction qui récupère les data de l'api et les initialise 
+        
         getAllChampions();
     }, []);
-    const championId = "Aatrox";
-    // const championId = championList.find((??) , ?? == ?? )
-
+    
+    const championId = championList.find(champion => champion.id == "Aatrox")
+    console.log(championId)
     useEffect( ()=>{
-        const getChampion = async() => {
-            // console.log(championId);
+
+        if(!championId){
+            const getChampion = async() => {
+            
             const CHAMPION_URL = `https://ddragon.leagueoflegends.com/cdn/15.23.1/data/fr_FR/champion/${championId}.json`;
-            // console.log(CHAMPION_URL);
+            
             try {
                 const championData = await axios.get(CHAMPION_URL);
-                const champion:Champion[] = Object.values(championData.data);
-                setChampion(champion);
+                const championAlone = Object.values(championData.data.data);
+                setChampion(championAlone);
                 
             } catch (error) {
                 console.log("Erreur lors du chargement du champion", error);
             }    
         }
         getChampion();
-    },[]);
+        };
+        
+    },[championId]);
     return(
         <div className="flex-column">
-            <p className="text fontUp my-5">Nom du Champion : </p>
-            <p className="text fontUp ">Titre du Champion : </p>
-            <p className="text fontUp ">Histoire du Champion : </p>
-            <div className="text flex-column fontUp">
-                <p >Passif du Champion: </p>
-                <p className="d-flex justify-content-center">Passif</p>
+            <p className="text fontUp my-5">Nom du Champion : <span className="championName  d-flex justify-content-center"> {champion?.name}Nom</span> </p>
+            <p className="text fontUp ">Titre du Champion : <span className="d-flex justify-content-center">{champion?.title} Titre</span> </p>
+            <p className="text fontUp ">Histoire du Champion : <span className="d-flex justify-content-center">{champion?.lore}</span> </p>
+            <div className="text fontUp">
+                <p >Passif du Champion: <span className="d-flex justify-content-center">{champion?.passive.name}</span> </p>
+                <p className="d-flex justify-content-center">{champion?.passive.description}</p>
             </div>
         </div>
     
     )
 }
-// <span className="championName  d-flex justify-content-center"> {champion.name}Nom</span>
-// <span className="d-flex justify-content-center">{champion.title} Titre</span>
-// <span className="d-flex justify-content-center">Histoire</span>
-// <span className="d-flex justify-content-center">{champion.passive.name}nom passif</span>
-// {champion.passive.description}
